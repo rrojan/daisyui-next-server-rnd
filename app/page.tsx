3efb8ad934c391b7b3b056f3c0a4939e9cb35a22
Rojan Rajbhandari <rrojan20@gmail.com>
2023-10-18 21:35:12 +0545

    fix: remove repeated btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
 import Link from "next/link"
 
 export default function Home() {
-  return (
-    <main className="px-8 py-4">
-      <h1>
-        <Link className="btn btn-primary" href="/users">
-          User Management
-        </Link>
-      </h1>
-    </main>
-  )
+  return <main className="px-8 py-4"></main>
 }
